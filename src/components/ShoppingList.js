dd88e819e8e105a4c9a4f6e667ac3f27e33ab079
i-prince <molatonny@gmail.com>
2023-09-20 02:39:48 +0300

    Completed Assignment

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,38 +1,51 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
-  const [selectedCategory, setSelectedCategory] = useState("All");
-
-  function handleCategoryChange(event) {
-    // event.target.value will be the value selected by the user
-    setSelectedCategory(event.target.value);
+class ShoppingList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedCategory: "all",
+    };
   }
 
-  // we want to filter the items to only display the ones based on the selected category
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  handleCategoryChange(event) {
+    this.setState({ selectedCategory: event.target.value });
+  }
 
-    return item.category === selectedCategory;
-  });
+  render() {
+    const { items } = this.props;
+    const { selectedCategory } = this.state;
 
-  return (
-    <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter" onChange={handleCategoryChange}>
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
+    // Filter items based on the selected category
+    const filteredItems = selectedCategory === "all"
+      ? items
+      : items.filter(item => item.category === selectedCategory);
+
+    return (
+      <div className="ShoppingList">
+        <div className="category-dropdown">
+          <label htmlFor="category">Select Category:</label>
+          <select
+            id="category"
+            name="category"
+            value={selectedCategory}
+            onChange={(event) => this.handleCategoryChange(event)}
+          >
+            <option value="all">All</option>
+            <option value="category1">Category 1</option>
+            <option value="category2">Category 2</option>
+            {/* Add more options for other categories */}
+          </select>
+        </div>
+        <ul className="Items">
+          {filteredItems.map((item, index) => (
+            <Item key={index} name={item.name} category={item.category} />
+          ))}
+        </ul>
       </div>
-      <ul className="Items">
-        {itemsToDisplay.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
-      </ul>
-    </div>
-  );
+    );
+  }
 }
 
 export default ShoppingList;
